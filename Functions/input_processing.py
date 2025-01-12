33f448990c6942ad42a9cc976e143fe1375c8ef1
julie <juliezoe98@googlemail.com>
2025-01-12 18:13:42 +0100

    working version 1

--- Functions/input_processing.py
+++ Functions/input_processing.py
@@ -1,45 +1,33 @@
-from geopandas import geopandas as gpd
-
-# read polygon
-# reproject polygon
-# intersect polygon
-# create output list with selected tiles
-
-path = "C:/Users/julie/Documents/Julia\Master GeoInfSpat/ITSP/ITSP-Project/data/testpolygon.shp"
-
-shapefile = gpd.read_file(path)
-
-print(shapefile.crs)
-print(list(shapefile.bounds))
+import geopandas as gpd
 
 
-class InputProcessing:
+class ShapefileInputProcessing:
     def __init__(self, file_path):
         self.input_file = file_path
-
-
-    def read_input_file(self):
         self.shapefile = gpd.read_file(self.input_file)
+        self.crs = self.shapefile.crs
+
+    def reproject(self):
+        if self.shapefile.crs.to_epsg() != 4326:
+            self.shapefile = self.shapefile.to_crs('EPSG:4326')
         return self.shapefile
 
-    def get_crs(self):
-        self.crs = shapefile.crs
-        return self.crs
-
-    def reproject(self, crs):
-        if self.crs != 4326:
-            self.shapefile_4326 = shapefile.to_crs({'init': 'epsg:4326'})
-        return self.shapefile_4326
-
     def get_bbox(self):
-        if self.crs != 4326:
-            self.bbox = self.shapefile_4326.bounds
-        else:
-            self.bbox = self.shapefile.bounds
-        return self.bbox
+        bbox = self.shapefile.bounds
+        return bbox
+
+    def convert_to_geojson(self, output_path='polygon.geojson'):
+        self.shapefile.to_file(output_path, driver='GeoJSON')
+        return output_path
 
 
+'''
+# Example
+file_path = "C:/Users/julie/Documents/Julia/Master GeoInfSpat/ITSP/ITSP-Project/data/testpolygon.shp"
 
+shapefile = ShapefileInputProcessing(file_path)
+shapefile.reproject()
+geojson = shapefile.convert_to_geojson()
 
-
-
+print(f"saved as: {geojson}")
+'''
