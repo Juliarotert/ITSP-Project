c5c19740fdfcd6b92ef3b679914f4facff8b960c
julie <juliezoe98@googlemail.com>
2025-01-16 20:40:35 +0100

    updates

--- Functions/output_download.py
+++ Functions/output_download.py
@@ -1,61 +1,35 @@
 import functions.api_interaction as ai
 import requests
-import time
-from multiprocessing import cpu_count
-from multiprocessing.pool import ThreadPool
+
+from pathlib import Path
 
 
 class OutputDownloader:
-    def __init__(self, url_dict, output_path):
+    def __init__(self, url_dict, output_folder):
         self.dict = url_dict
-        self.output_path = output_path
+        self.output_folder = output_folder
 
-    # download part based on https://towardsdatascience.com/use-python-to-download-multiple-files-or-urls-in-parallel-1759da9d6535
-    # function to download urls
-    def download_url(self, args):
-        t0 = time.time()
-        url, fn = args[0], args[1]
-        try:
-            r = requests.get(url)
-            with open(fn, 'wb') as f:
-                f.write(r.content)
-            return (url, time.time() - t0)
-        except Exception as e:
-            print('Exception in download_url():', e)
-            return (url, None)
-
-    # function for parallel downloads
-    def download_parallel(self, args):
-        cpus = cpu_count()
-        results = ThreadPool(cpus - 1).imap_unordered(self.download_url, args)
-        for result in results:
-            if result[1] is not None:
-                print(f"Downloaded: {result[0]} in {result[1]:.2f} seconds")
-            else:
-                print(f"Failed to download: {result[0]}")
-
-    # function to execute downloads to path
-    def download_tif_files(self):
-        file_names = []
-        urls = []
-
+    # function to download the files as id-name
+    def download_dict_tif(self):
         for id, url in self.dict.items():
-            file_names.append(f"{self.output_path}/{id}.tif")
-            urls.append(url)
+            response = requests.get(url)
+            file_path = f"{self.output_folder}/{id}.tif"
+            with open(file_path, 'wb') as f:
+                f.write(response.content)
 
-        inputs = zip(urls, file_names)
+            print(f"downloaded to {file_path}")
 
-        self.download_parallel(inputs)
+        return print(f"finished download to {self.output_folder}")
 
 
 
-# URL-Dictionary aus API-Interaction (korrigieren falls nötig)
+# Example
+# url-dict from the API-Interaction
 dict = ai.url_dict
 
-# Korrigierter Dateipfad
-output_path = r"C:\Users\julie\Documents\Julia\Master GeoInfSpat\ITSP\ITSP-Project\data\Output_tests"
-output_path = output_path.replace("\\", "/")
+# output filder path
+output_path = Path(r"C:\Users\julie\Documents\Julia\Master GeoInfSpat\ITSP\ITSP-Project\data\output test")
 
-# Downloader-Objekt erstellen und Dateien herunterladen
+# download urls
 od = OutputDownloader(dict, output_path)
-od.download_tif_files()
+od.download_dict_tif()
